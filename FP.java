bd85b7b66c02c5520b9c62975d8f6964863a267b
TejaGollapudi <tejagtz10@gmail.com>
2017-04-29 00:30:22 +0530

    metioned functionality of each code segment

--- FP.java
+++ FP.java
@@ -10,17 +10,17 @@ public class FP {
 
 	private static final String FILENAME = "file.txt";
 
-
+//method to print tree by taking the root node by recurssion.
 	public static void printtree(node n)
 	{
 		System.out.println("node " +n.name+" count "+ n.count);
 		ArrayList<node> c=n.getchild();
-		if(!c.isEmpty())
+		if(!c.isEmpty())//checks if the current node has childnodes or not
 		{
 			Iterator<node> iterator = c.iterator();
 			while(iterator.hasNext())
 			{
-				printtree(iterator.next());
+				printtree(iterator.next());//for each childnode call the function.
 			}
 		}
 		else
@@ -49,13 +49,14 @@ int i;
 
 		while ((CurrentLine = br.readLine()) != null) {
 				String[] s=CurrentLine.split(",",2);	
+				//splits data into Transaction ID and transactions
 				String [] b=s[1].split(",");
 				ArrayList<String> e=new ArrayList<String>();
 				for(i=0;i<b.length;i++)
 				{
-					e.add(b[i]);
+					e.add(b[i]); //adds each element of transaction as an element of array list./
 				}
-				ar.add(e);
+				ar.add(e);//adds the whole array list into an outer array list which contains all transactions
 				
 				}
 			
@@ -66,6 +67,7 @@ int i;
 	{
 	System.out.println(ar.get(i).toString());
 		ArrayList<String> s=ar.get(i);
+		//creates a key value pair of number of items and thier occurance
 		for(j=0;j<s.size();j++)
 			{
 				if(map.containsKey(s.get(j)))
@@ -102,7 +104,7 @@ for(i=0;i<ar.size();i++)
 {
 ArrayList<String> a;
 a=ar.get(i);
-
+//sorts the transaction arraylist based on the number of ocurances of each item
 Collections.sort(a, new Comparator<String>() {
         @Override
         public int compare(String s1, String s2)
@@ -112,16 +114,16 @@ Collections.sort(a, new Comparator<String>() {
         }
     });
 
-data.add(a);
+data.add(a); // adds the sorted transactions into a new array list called data
 }
 node parent=new node();
 parent.name="parent";
-for(i=0;i<data.size();i++)
+for(i=0;i<data.size();i++)//goes through each transaction set
 {
 System.out.println("data -----------"+data.get(i).toString());
 Iterator<String> iterator = data.get(i).iterator();
-node check=parent;
-while(iterator.hasNext()){
+node check=parent;//check is the base node used to compare
+while(iterator.hasNext()){//iterator next gets each individual element in the transaction
   String s= iterator.next();
   System.out.println("Iterator "+s);
   int f=0;
@@ -129,17 +131,18 @@ while(iterator.hasNext()){
  // System.out.println("outer if condition entered ");
   	for(k=0;k<check.child.size();k++)
   	{
-  		if(check.child.get(k).name.equals(s))
+  		if(check.child.get(k).name.equals(s)) //if there exists a child of the check node with same name as item
   		{
-  			check=check.child.get(k);
-  			check.inccount();
+  			check=check.child.get(k);//set check as the child of current check node
+
+  			check.inccount();//increase the count of the node as it has been traversed
   			System.out.println("if condition entered //traversal of tree performed");
   			f=1;
   		}
   	}
   
-  if(f==0){
-  	node n=new node();
+  if(f==0){//if no such child node exists then create a new child node and add it as a child of checknode and make check node its parent.
+  	node n=new node(); 
   	n.name=s;
   	n.setparent(check);
   	check.setchild(n);
